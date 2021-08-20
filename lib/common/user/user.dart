73c653b2aba9e23797d60dcfae440eb1ec114d47
Manuel Eduardo Nascimento Antunes <mena1@aluno.ifal.edu.br>
2021-08-20 15:23:16 -0300

    feat: create user class

---
+++ lib/common/user/user.dart
@@ -0,0 +1,11 @@
+class User {
+  User(
+      {required this.name,
+      required this.email,
+      required this.photoUrl,
+      required this.id});
+  String name;
+  String email;
+  String photoUrl;
+  String id;
+}
