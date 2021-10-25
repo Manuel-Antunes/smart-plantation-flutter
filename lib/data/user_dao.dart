bfcd3761d2c6ea2cb3e7d864bce6ba49663aa70f
Manuel Eduardo Nascimento Antunes <mena1@aluno.ifal.edu.br>
2021-10-25 00:09:44 -0300

    feat: add store user query

--- lib/data/user_dao.dart
+++ lib/data/user_dao.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:smart_plantation/common/user/user.dart';
 import 'package:smart_plantation/data/connection.dart';
 
@@ -6,5 +8,16 @@ class UserDao {
     var db = await Connection.getConnection();
     final result = await db.rawQuery("SELECT * FROM user");
     return User.fromJson(result[0]);
+  }
+
+  FutureOr<void> storeUser(User user) async {
+    var db = await Connection.getConnection();
+    try {
+      await db.rawQuery(
+          "INSERT INTO user  (id, name, email, photoUrl) VALUES (?, ?, ?, ?)",
+          [user.id, user.name, user.email, user.photoUrl]);
+    } catch (error) {
+      throw Exception('error while creating user');
+    }
   }
 }
