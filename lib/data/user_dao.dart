afc6462bd7fbc30c9980445312687123fde48148
Manuel Eduardo Nascimento Antunes <mena1@aluno.ifal.edu.br>
2021-12-31 04:07:41 -0300

    feat: add logout query

--- lib/data/user_dao.dart
+++ lib/data/user_dao.dart
@@ -4,10 +4,12 @@ import 'package:smart_plantation/common/user/user.dart';
 import 'package:smart_plantation/data/connection.dart';
 
 class UserDao {
-  Future<User> getUser() async {
+  Future<User?> getUser() async {
     var db = await Connection.getConnection();
     final result = await db.rawQuery("SELECT * FROM user");
-    return User.fromJson(result[0]);
+    if (result.isNotEmpty) {
+      return User.fromJson(result[0]);
+    }
   }
 
   FutureOr<void> storeUser(User user) async {
@@ -18,6 +20,15 @@ class UserDao {
           [user.id, user.name, user.email, user.photoUrl]);
     } catch (error) {
       throw Exception('error while creating user');
+    }
+  }
+
+  FutureOr<void> removeUser(User user) async {
+    var db = await Connection.getConnection();
+    try {
+      await db.rawQuery("DELETE * FROM user");
+    } catch (error) {
+      throw Exception('error while deleting user');
     }
   }
 }
