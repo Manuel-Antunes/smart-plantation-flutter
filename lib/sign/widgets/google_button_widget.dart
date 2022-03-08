78e5c194b297dce1cdd767a8d41d2766fe43da81
Manuel Eduardo Nascimento Antunes <mena1@aluno.ifal.edu.br>
2022-03-08 02:30:40 -0300

    fix: navigate after login

--- lib/sign/widgets/google_button_widget.dart
+++ lib/sign/widgets/google_button_widget.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:flutter_signin_button/flutter_signin_button.dart';
+import 'package:smart_plantation/home/home_page.dart';
 
 class GoogleButtonWidget extends StatelessWidget {
   const GoogleButtonWidget({Key? key}) : super(key: key);
@@ -13,7 +14,9 @@ class GoogleButtonWidget extends StatelessWidget {
         child: SignInButton(
           Buttons.Google,
           text: "Sign up with Google",
-          onPressed: _onPressed,
+          onPressed: () {
+            _onPressed(context);
+          },
           padding: EdgeInsets.symmetric(vertical: 15),
           // minimumSize: const Size(double.infinity, 30),]
         ));
@@ -21,7 +24,7 @@ class GoogleButtonWidget extends StatelessWidget {
 
   Future<String?> signInwithGoogle() async {}
 
-  Future<void> _onPressed() async {
+  Future<void> _onPressed(BuildContext context) async {
     try {
       final GoogleSignInAccount? googleSignInAccount =
           await GoogleSignIn().signIn();
@@ -34,7 +37,12 @@ class GoogleButtonWidget extends StatelessWidget {
       UserCredential crd =
           await FirebaseAuth.instance.signInWithCredential(credential);
       String idToken = await crd.user!.getIdToken();
-      print(idToken);
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => HomePage(),
+        ),
+      );
     } on FirebaseAuthException catch (e) {
       print(e.message);
       // throw e;
